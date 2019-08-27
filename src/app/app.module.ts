4566eb750ca8477a16576a374e1f1a3be82ba13b
Rutenis <r.raila@keepthinking.it>
2019-08-27 17:58:42 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarDropdownComponent } from './navbar/navbar-dropdown/navbar-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarDropdownComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/navbar-dropdown/navbar-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-dropdown',
+  templateUrl: './navbar-dropdown.component.html',
+  styleUrls: ['./navbar-dropdown.component.scss']
+})
+export class NavbarDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
